0bdb9d7110362bcdbecb0635e771ee65e01ce8df
Taylor Buckner <taylora.buckner@gmail.com>
2020-02-11 15:22:51 -0600

    feat(tappable): place box near mouse click position

--- src/app/core/components/tappable-area/tappable-area.component.ts
+++ src/app/core/components/tappable-area/tappable-area.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit } from '@angular/core';
 import { SamplesService } from '../../samples.service';
+
+import * as p5 from 'p5';
 
 @Component({
   selector: 'app-tappable-area',
@@ -7,16 +9,40 @@ import { SamplesService } from '../../samples.service';
   styleUrls: ['./tappable-area.component.scss']
 })
 export class TappableAreaComponent implements OnInit {
+  p5: p5;
+  mouseX: number;
+  mouseY: number;
 
-  constructor(private sample: SamplesService) { }
+  constructor(private sample: SamplesService,
+              private el: ElementRef) { }
 
   ngOnInit() {
+    this.p5 = new p5((p: p5) => {
+      const x = 100;
+      const y = 100;
+      const dimensions = (this.el.nativeElement as HTMLElement).getBoundingClientRect();
+
+      p.setup = () => {
+        p.createCanvas(dimensions.width, dimensions.height).id('overlay-canvas');
+      };
+
+      p.draw = () => {
+        p.background(24);
+        p.fill(255);
+        if (!this.mouseX || !this.mouseY) {
+          return;
+        }
+        p.rect(this.mouseX, this.mouseY, 25, 25);
+      };
+    }, this.el.nativeElement.querySelector('.tappable-area__canvas-outlet'));
   }
 
   public onClick(event: MouseEvent) {
-    console.warn(event.screenX, event.screenY);
+    this.mouseX = event.clientX;
+    this.mouseY = event.clientY;
     const path = this.sample.getRandomSample();
     new Audio(path).play();
+    this.p5.redraw();
   }
 
 }
