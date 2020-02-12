db82cd440525d31f93a4ad593bee90f692343cc9
Taylor Buckner <taylora.buckner@gmail.com>
2020-02-12 08:09:57 -0600

    feat(p5): added basic animation POC

--- src/app/core/components/tappable-area/tappable-area.component.ts
+++ src/app/core/components/tappable-area/tappable-area.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ElementRef, AfterViewInit } from '@angular/core';
 import { SamplesService } from '../../samples.service';
 
 import * as p5 from 'p5';
+import { Subject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tappable-area',
@@ -12,14 +13,13 @@ export class TappableAreaComponent implements OnInit {
   p5: p5;
   mouseX: number;
   mouseY: number;
+  bursts: Burst[] = [];
 
   constructor(private sample: SamplesService,
               private el: ElementRef) { }
 
   ngOnInit() {
     this.p5 = new p5((p: p5) => {
-      const x = 100;
-      const y = 100;
       const dimensions = (this.el.nativeElement as HTMLElement).getBoundingClientRect();
 
       p.setup = () => {
@@ -28,21 +28,67 @@ export class TappableAreaComponent implements OnInit {
 
       p.draw = () => {
         p.background(24);
-        p.fill(255);
-        if (!this.mouseX || !this.mouseY) {
-          return;
+
+        for (const burst of this.bursts) {
+          burst.show();
+          burst.animate();
+
+          if (burst.invisible) {
+            this.bursts = this.bursts.filter(e => e !== burst);
+          }
         }
-        p.rect(this.mouseX, this.mouseY, 25, 25);
       };
     }, this.el.nativeElement.querySelector('.tappable-area__canvas-outlet'));
   }
 
   public onClick(event: MouseEvent) {
-    this.mouseX = event.clientX;
-    this.mouseY = event.clientY;
-    const path = this.sample.getRandomSample();
-    new Audio(path).play();
-    this.p5.redraw();
+    this.playRandomSample();
+    const nextBurst = new Burst(this.p5, event.clientX, event.clientY);
+    this.bursts.push(nextBurst);
   }
 
+
+  private playRandomSample() {
+    const path = this.sample.getRandomSample();
+    new Audio(path).play();
+  }
+}
+
+class Burst {
+  public invisible = false;
+  private x: number;
+  private y: number;
+  private radius = 20;
+  private ctx: p5;
+  private color: p5.Color;
+  private alpha = 255;
+  private radiusRate: number;
+  private alphaRate: number;
+
+
+  constructor(sketchInstance: p5, xPos: number, yPos: number, radiusRate = 1, alphaRate = 2) {
+    this.ctx = sketchInstance;
+    this.x = xPos;
+    this.y = yPos;
+    this.radiusRate = radiusRate;
+    this.alphaRate = alphaRate;
+    this.color = this.ctx.color(100, 50, 150);
+  }
+
+  show() {
+    this.ctx.noStroke();
+    this.ctx.fill(this.color);
+    this.ctx.ellipse(this.x, this.y, this.radius * 2);
+    this.ctx.redraw();
+  }
+
+  animate() {
+    this.radius += this.radiusRate;
+    this.alpha -= this.alphaRate;
+    this.color.setAlpha(this.alpha);
+
+    if (this.ctx.alpha(this.color) < 1) {
+      this.invisible = true;
+    }
+  }
 }
